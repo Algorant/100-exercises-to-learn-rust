ed374378c78a72629a2e75fd9a6ca317cd751173
Algorant <Skantastico@gmail.com>
2024-10-26 00:00:04 -0400

    got stuck at end of ch 3

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,64 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+#[derive(Debug)]
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Option<Self> {
+        if product_name.is_empty() || product_name.len() > 300 || quantity == 0 || unit_price == 0 {
+            return None;
+        }
+
+        Some(Self {
+            product_name,
+            quantity,
+            unit_price,
+        })
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) -> bool {
+        if product_name.is_empty() || product_name.len() > 300 {
+            return false;
+        }
+        self.product_name = product_name;
+        true
+    }
+
+    pub fn quantity(&self) -> u32 {
+        self.quantity
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) -> bool {
+        if quantity == 0 {
+            return false;
+        }
+        self.quantity = quantity;
+        true
+    }
+
+    pub fn unit_price(&self) -> u32 {
+        self.unit_price
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) -> bool {
+        if unit_price == 0 {
+            return false;
+        }
+        self.unit_price = unit_price;
+        true
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+}
