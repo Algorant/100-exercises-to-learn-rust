0e1844ad83fa7c43a4b489894010dc30c2f6d755
Algorant <Skantastico@gmail.com>
2025-02-05 23:54:14 -0500

    completed chapter 4

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,40 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    defused: bool,
+    value: u32,
+    description: String,
+}
+
+impl Default for DropBomb {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        Self {
+            defused: false,
+            value: 42,
+            description: "a bomb".to_string(),
+        }
+    }
+
+    pub fn defuse(&mut self) {
+        self.defused = true;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.defused {
+            panic!("Bomb was not defused!");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
