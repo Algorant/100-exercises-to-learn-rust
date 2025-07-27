2210a42cb58adc6a0dfb4f88e029ae85d8edf58b
Algorant <Skantastico@gmail.com>
2025-07-27 00:32:53 -0400

    did 10

--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -35,7 +35,16 @@ impl TicketStoreClient {
         Ok(response_receiver.recv().unwrap())
     }
 
-    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {}
+    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {
+        let (response_sender, response_receiver) = sync_channel(1);
+        self.sender
+            .try_send(Command::Update {
+                patch: ticket_patch,
+                response_channel: response_sender,
+            })
+            .map_err(|_| OverloadedError)?;
+        Ok(response_receiver.recv().unwrap())
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -85,7 +94,20 @@ pub fn server(receiver: Receiver<Command>) {
                 patch,
                 response_channel,
             }) => {
-                todo!()
+                // Apply the patch to the store
+                let result = store.patch_ticket(patch);
+
+                // Send response back - we ignore send errors since the client might have disconnected
+                match result {
+                    Ok(()) => {
+                        let _ = response_channel.send(());
+                    }
+                    Err(_) => {
+                        // For now, we still send () even if patch fails
+                        // In a real app, you might want to return Result<(), PatchError>
+                        let _ = response_channel.send(());
+                    }
+                }
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
